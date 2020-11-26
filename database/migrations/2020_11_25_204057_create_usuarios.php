e18ddd942bfd7ab90b2b21bac9aa0668d60776b1
Marisol Hernandez <64989938+Marisolhr@users.noreply.github.com>
2020-11-25 17:11:54 -0700

    Add files via upload

---
+++ database/migrations/2020_11_25_204057_create_usuarios.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->increments('idUsuario');
+            $table->string('user', 200);
+            $table->string('password', 255);
+            $table->string('key', 255);
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('usuarios', function (Blueprint $table) {
+            Schema::dropIfExists('usuarios');
+        });
+    }
+}
